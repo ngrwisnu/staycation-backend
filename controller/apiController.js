b238c3b47578fee5028a2b583cf76c6eb28a9794
ngrwisnu <adiprayogongrwisnu@gmail.com>
2022-10-28 08:52:01 +0700

    created API for Booking Page and created postman doc for the API

--- controller/apiController.js
+++ controller/apiController.js
@@ -3,6 +3,8 @@ const Traveler = require("../models/Booking");
 const Landscapes = require("../models/Activity");
 const Category = require("../models/Category");
 const Bank = require("../models/Bank");
+const Booking = require("../models/Booking");
+const Member = require("../models/Member");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -122,7 +124,7 @@ module.exports = {
         },
       ];
 
-      res.status(200).json({
+      return res.status(200).json({
         hero: {
           travelers: traveler.length,
           landscapes: landscapes.length,
@@ -131,12 +133,11 @@ module.exports = {
         landscapes,
         mostChosen,
         category,
-        testimonial,
         stories,
       });
     } catch (error) {
       console.log(error);
-      res.status(500).json({ message: "Internal Server ERROR" });
+      return res.status(500).json({ message: "Internal Server ERROR" });
     }
   },
 
@@ -166,6 +167,94 @@ module.exports = {
         bank,
         testimonial,
       });
-    } catch (error) {}
+    } catch (error) {
+      res.status(500).json({ message: "Internal Server ERROR" });
+    }
+  },
+
+  bookingPage: async (req, res) => {
+    const {
+      idItem,
+      duration,
+      // price,
+      startDate,
+      endDate,
+      firstName,
+      lastName,
+      email,
+      phone,
+      accountHolder,
+      bankFrom,
+    } = req.body;
+
+    console.log(req.file);
+
+    if (!req.file) {
+      res.status(404).json({ message: "Image not found" });
+    }
+
+    if (
+      idItem === undefined ||
+      duration === undefined ||
+      // price === undefined ||
+      startDate === undefined ||
+      endDate === undefined ||
+      firstName === undefined ||
+      lastName === undefined ||
+      email === undefined ||
+      phone === undefined ||
+      accountHolder === undefined ||
+      bankFrom === undefined
+    ) {
+      return res.status(404).json({ message: "Please fill all input field" });
+    }
+
+    const item = await Item.findOne({ _id: idItem });
+
+    if (!item) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    item.sumBooking += 1;
+
+    await item.save();
+
+    let total = item.price * duration;
+    let tax = total * 0.1;
+
+    const invoice = Math.floor(1000000 + Math.random() * 9000000);
+
+    const member = await Member.create({
+      firstName,
+      lastName,
+      email,
+      phone,
+    });
+
+    const newBooking = {
+      invoice,
+      startDate,
+      endDate,
+      total: (total += tax),
+      itemId: {
+        _id: item.id,
+        title: item.title,
+        price: item.price,
+        duration: duration,
+      },
+      memberId: member.id,
+      payments: {
+        paymentProof: `images/${req.file.filename}`,
+        bankFrom: bankFrom,
+        accountHolder: accountHolder,
+      },
+    };
+
+    const booking = await Booking.create(newBooking);
+
+    return res.status(201).json({
+      message: "Your book has been created",
+      booking,
+    });
   },
 };
